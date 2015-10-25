a300971b5fa5ce9024b7dda109c634aa2740622a
Francis Castillo <franciselcaliente@gmail.com>
2015-10-25 15:06:12 +0100

    1. Agregando Librerias BLL y DAL

--- DAL/Class1.cs
+++ DAL/Class1.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL
 {
-    public class ConexionDB
+    public class Class1
     {
     }
 }
