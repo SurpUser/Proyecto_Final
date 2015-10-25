53612c2083129c42e388a3d9dcb59c6e61e7f273
Francis Castillo <franciselcaliente@gmail.com>
2015-10-25 15:46:16 +0100

    Agregando Clase ConexionDB

---
+++ DAL/ConexionDB.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DAL
+{
+    public class ConexionDB
+    {
+        private SqlConnection con;
+        private SqlCommand Cmd;
+
+        public ConexionDB()
+        {
+            con = new SqlConnection("Data Source=ROOT-PC\\SURPUSER;Initial Catalog=StrongerDB;Integrated Security=True");
+            Cmd = new SqlCommand();
+        }
+
+        public bool Ejecutar(String ComandoSql)
+        {
+            bool retorno = false;
+
+            try
+            {
+                con.Open();
+                Cmd.Connection = con;
+                Cmd.CommandText = ComandoSql;
+                Cmd.ExecuteNonQuery();
+                retorno = true;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return retorno;
+        }
+
+        public DataTable ObtenerDatos(String ComandoSql)
+        {
+
+            SqlDataAdapter adapter;
+            DataTable dt = new DataTable();
+
+            try
+            {
+                con.Open();
+                Cmd.Connection = con;
+                Cmd.CommandText = ComandoSql;
+
+                adapter = new SqlDataAdapter(Cmd);
+                adapter.Fill(dt);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return dt;
+        }
+    }
+}
