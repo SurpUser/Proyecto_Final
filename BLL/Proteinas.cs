e8cda70d164bcdd31139bf920b326b76d3ddc834
edwinh150 <edwinh2410@gmail.com>
2015-11-07 14:40:04 -0400

    1. Creando clase proteinas 2. Terminando metodos de la clase proteinas 3. Probando registro de proteinas

---
+++ BLL/Proteinas.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class Proteinas : ClaseMaestra
+    {
+        ConexionDB con = new ConexionDB();
+
+        public int ProteinaId { get; set; }
+
+        public string  Nombre { get; set; }
+
+        public double Precio { get; set; }
+
+        public double ITBS { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public double Costo { get; set; }
+
+        public Proteinas()
+        {
+            this.ProteinaId = 0;
+            this.Nombre = "";
+            this.Precio = 0.0;
+            this.ITBS = 0.0;
+            this.Cantidad = 0;
+            this.Costo = 0.0;
+        }
+
+        public Proteinas(int ProteinaID, string Nombre, double Precio, double Itbs, int Cantidad, double Costo)
+        {
+            this.ProteinaId = ProteinaID;
+            this.Nombre = Nombre;
+            this.Precio = Precio;
+            this.ITBS = Itbs;
+            this.Cantidad = Cantidad;
+            this.Costo = Costo;
+        }
+
+        public override bool Insertar()
+        {
+            bool retorno = false;
+            try
+            {
+                retorno = con.Ejecutar(string.Format("insert into Proteinas (Nombre, Precio, ITBS, Cantidad, Costo) values ('{0}',{1},{2},{3},{4})", this.Nombre, this.Precio, this.ITBS, this.Cantidad, this.Costo));
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            return retorno;
+        }
+
+        public override bool Editar()
+        {
+            bool retorno = false;
+            try
+            {
+                retorno = con.Ejecutar(string.Format("update Proteinas set Nombre = '{0}', Precio = {1}, ITBS = {2}, Cantidad = {3}, Costo = {4} where ProteinaId = {5}", this.Nombre, this.Precio, this.ITBS, this.Cantidad, this.Costo, this.ProteinaId));
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            return retorno;
+        }
+
+        public override bool Eliminar()
+        {
+            bool retorno = false;
+            try
+            {
+                retorno = con.Ejecutar(string.Format("delete from Proteinas where ProteinaId = {0}", this.ProteinaId));
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            return retorno;
+        }
+
+        public override bool Buscar(int IdBuscado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataTable Listado(string Campos, string Condicion, string Orden)
+        {
+            return con.ObtenerDatos("select " + Campos + " from Proteinas " + Condicion + " " + Orden);
+        }
+    }
+}
--- StrongerGym/Registros/ProteinaRegistrosForm.cs
+++ StrongerGym/Registros/ProteinaRegistrosForm.cs
@@ -7,14 +7,125 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BLL;
 
 namespace StrongerGym.Registros
 {
     public partial class ProteinaRegistrosForm : Form
     {
+        Proteinas proteina;
+        int Convertir = 0;
+        double Convertir2 = 0.0;
+        bool Resultado;
+
         public ProteinaRegistrosForm()
         {
             InitializeComponent();
+            proteina = new Proteinas();
+        }
+
+        private void Nuevobutton_Click(object sender, EventArgs e)
+        {
+            ProteinaIdtextBox.Clear();
+            NombretextBox.Clear();
+            PreciotextBox.Clear();
+            ITBStextBox.Clear();
+            CostotextBox.Clear();
+            CantidadtextBox.Clear();
+            TiposProteinacomboBox.SelectedIndex = 0;
+            TiposProteinalistBox.Items.Clear();
+        }
+
+        private void Guardarbutton_Click(object sender, EventArgs e)
+        {
+            
+            if (ProteinaIdtextBox.Text.Length == 0)
+            {        
+                Resultado = Int32.TryParse(ProteinaIdtextBox.Text, out Convertir);
+
+                proteina.ProteinaId = Convertir;
+
+                proteina.Nombre = NombretextBox.Text;
+
+                Resultado = Double.TryParse(PreciotextBox.Text, out Convertir2);
+
+                proteina.Precio = Convertir2;
+
+                Resultado = Double.TryParse(ITBStextBox.Text, out Convertir2);
+
+                proteina.ITBS = Convertir2;
+
+                Resultado = Int32.TryParse(CantidadtextBox.Text, out Convertir);
+
+                proteina.Cantidad = Convertir;
+
+                Resultado = Double.TryParse(CostotextBox.Text, out Convertir2);
+
+                proteina.Costo = Convertir2;
+
+                if (proteina.Insertar())
+                {
+                    MessageBox.Show("Se guardo correctamente");
+                }
+                else
+                {
+                    MessageBox.Show("No se guardo");
+                }
+            }
+            else
+            {
+                Resultado = Int32.TryParse(ProteinaIdtextBox.Text, out Convertir);
+
+                proteina.ProteinaId = Convertir;
+
+                proteina.Nombre = NombretextBox.Text;
+
+                Resultado = Double.TryParse(PreciotextBox.Text, out Convertir2);
+
+                proteina.Precio = Convertir2;
+
+                Resultado = Double.TryParse(ITBStextBox.Text, out Convertir2);
+
+                proteina.ITBS = Convertir2;
+
+                Resultado = Int32.TryParse(CantidadtextBox.Text, out Convertir);
+
+                proteina.Cantidad = Convertir;
+
+                Resultado = Double.TryParse(CostotextBox.Text, out Convertir2);
+
+                proteina.Costo = Convertir2;
+
+                if (proteina.Editar())
+                {
+                    MessageBox.Show("Se edito correctamente");
+                }
+                else
+                {
+                    MessageBox.Show("No se edito");
+                }
+            }
+        }
+
+        private void Eliminarbutton_Click(object sender, EventArgs e)
+        {
+            Resultado = Int32.TryParse(ProteinaIdtextBox.Text, out Convertir);
+
+            proteina.ProteinaId = Convertir;
+
+            if (proteina.Eliminar())
+            {
+                MessageBox.Show("Se elimino correctamente");
+            }
+            else
+            {
+                MessageBox.Show("No se elimino");
+            }
+        }
+
+        private void Agregarbutton_Click(object sender, EventArgs e)
+        {
+            TiposProteinalistBox.Items.Add(TiposProteinacomboBox.Text);
         }
     }
 }
