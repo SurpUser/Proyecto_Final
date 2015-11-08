e14a2c879b138c950bbe4c8ea5d448104af25820
edwinh150 <edwinh2410@gmail.com>
2015-11-08 10:06:10 -0400

    nada

--- DAL/ConexionDB.cs
+++ DAL/ConexionDB.cs
@@ -16,6 +16,7 @@ namespace DAL
 
         public ConexionDB()
         {
+
             con = new SqlConnection("Data Source=ROOT-PC\\SURPUSER;Initial Catalog=StrongerDB;Integrated Security=True");
             Cmd = new SqlCommand();
         }
--- StrongerGym/Registros/ProteinaRegistrosForm.cs
+++ StrongerGym/Registros/ProteinaRegistrosForm.cs
@@ -15,6 +15,111 @@ namespace StrongerGym.Registros
         public ProteinaRegistrosForm()
         {
             InitializeComponent();
+            proteina = new Proteinas();
+        }
+
+        private void Nuevobutton_Click(object sender, EventArgs e)
+        {
+            ProteinaIdtextBox.Clear();
+            NombretextBox.Clear();
+            PreciotextBox.Clear();
+            ITBStextBox.Clear();
+            CostotextBox.Clear();
+            CantidadtextBox.Clear();
+            TiposProteinacomboBox.SelectedIndex = 0;
+            TiposProteinalistBox.Items.Clear();
+        }
+
+        private void Guardarbutton_Click(object sender, EventArgs e)
+        {
+            
+            if (ProteinaIdtextBox.Text.Length == 0)
+            {        
+                Resultado = Int32.TryParse(ProteinaIdtextBox.Text, out Convertir);
+
+                proteina.ProteinaId = Convertir;
+
+                proteina.Nombre = NombretextBox.Text;
+
+                Resultado = Double.TryParse(PreciotextBox.Text, out Convertir2);
+
+                proteina.Precio = Convertir2;
+
+                Resultado = Double.TryParse(ITBStextBox.Text, out Convertir2);
+
+                proteina.ITBS = Convertir2;
+
+                Resultado = Int32.TryParse(CantidadtextBox.Text, out Convertir);
+
+                proteina.Cantidad = Convertir;
+
+                Resultado = Double.TryParse(CostotextBox.Text, out Convertir2);
+
+                proteina.Costo = Convertir2;
+
+                if (proteina.Insertar())
+                {
+                    MessageBox.Show("Se guardo correctamente");
+                }
+                else
+                {
+                    MessageBox.Show("No se guardo");
+                }
+            }
+            else
+            {
+                Resultado = Int32.TryParse(ProteinaIdtextBox.Text, out Convertir);
+
+                proteina.ProteinaId = Convertir;
+
+                proteina.Nombre = NombretextBox.Text;
+
+                Resultado = Double.TryParse(PreciotextBox.Text, out Convertir2);
+
+                proteina.Precio = Convertir2;
+
+                Resultado = Double.TryParse(ITBStextBox.Text, out Convertir2);
+
+                proteina.ITBS = Convertir2;
+
+                Resultado = Int32.TryParse(CantidadtextBox.Text, out Convertir);
+
+                proteina.Cantidad = Convertir;
+
+                Resultado = Double.TryParse(CostotextBox.Text, out Convertir2);
+
+                proteina.Costo = Convertir2;
+
+                if (proteina.Editar())
+                {
+                    MessageBox.Show("Se edito correctamente");
+                }
+                else
+                {
+                    MessageBox.Show("No se edito");
+                }
+            }
+        }
+
+        private void Eliminarbutton_Click(object sender, EventArgs e)
+        {
+            Resultado = Int32.TryParse(ProteinaIdtextBox.Text, out Convertir);
+
+            proteina.ProteinaId = Convertir;
+
+            if (proteina.Eliminar())
+            {
+                MessageBox.Show("Se elimino correctamente");
+            }
+            else
+            {
+                MessageBox.Show("No se elimino");
+            }
+        }
+
+        private void Agregarbutton_Click(object sender, EventArgs e)
+        {
+            TiposProteinalistBox.Items.Add(TiposProteinacomboBox.Text);
         }
     }
 }
